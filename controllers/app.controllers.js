602a6fa288e4c7f137e743794d7cc740736feb1c
karolinakonior <karolina.koniorr@gmail.com>
2023-10-02 22:23:56 +0100

    Complete GET All Articles endpoint

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -33,7 +33,7 @@ exports.getArticleByID = (req, res, next) => {
 
 exports.getArticles = (req, res, next) => {
     fetchArticles().then((articles) => {
-            res.status(200).send({ articles })
+        res.status(200).send({ articles })
     })
     .catch((err) => {
         next(err);
@@ -50,11 +50,7 @@ exports.getArticleByID = (req, res, next) => {
     })
 }
 
-exports.getArticles = (req, res, next) => {
-    fetchArticles().then((articles) => {
-            res.status(200).send({ articles })
-    })
-    .catch((err) => {
-        next(err)
-    })
-}
+
+
+
+
