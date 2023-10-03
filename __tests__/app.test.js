fb142c7373771b689c51de5d8a024f9e22a990ab
karolinakonior <karolina.koniorr@gmail.com>
2023-10-03 10:52:00 +0100

    Implemented changes to tests and controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,4 @@
 const db = require('../db/connection');
-const { getTopic } = require('../controllers/app.controllers');
 const app = require('../app');
 const request = require("supertest");
 const seed = require('../db/seeds/seed');
@@ -32,9 +31,7 @@ describe('GET /api/topics', () => {
         .then((response) => {
             expect(response.body.topics.length).toBe(3);
             response.body.topics.forEach((topic) => {
-            expect(typeof topic.description).toBe('string');
-            expect(typeof topic.slug).toBe('string');
-            expect(response.body.topics[0]).toEqual(expect.objectContaining({
+            expect(topic).toEqual(expect.objectContaining({
                 description: expect.any(String),
                 slug: expect.any(String)
               }))
@@ -85,16 +82,16 @@ describe('GET /api/articles/:article_id', () => {
         return request(app)
         .get('/api/articles/2')
         .expect(200)
-        .then((response) => {
-            expect(response.body.article[0]).toEqual(expect.objectContaining({
+        .then((article) => {
+            expect(article.body.article).toEqual(expect.objectContaining({
                 article_id: 2,
-                title: expect.any(String),
-                topic: expect.any(String),
-                author: expect.any(String),
-                body: expect.any(String),
-                created_at: expect.any(String),
-                article_img_url: expect.any(String),
-                votes: expect.any(Number)
+                title: "Sony Vaio; or, The Laptop",
+                topic: "mitch",
+                author: "icellusedkars",
+                body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+                created_at: "2020-10-16T05:03:00.000Z",
+                article_img_url:  "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+                votes: 0
               }))
         })
     });
--- models/app.models.js
+++ models/app.models.js
@@ -12,6 +12,6 @@ exports.fetchArticleById = (articleID) => {
     return db.query(`SELECT * FROM articles
                     WHERE article_id = $1;`, [articleID])
     .then(({ rows }) => {
-        return rows;
+        return rows[0];
     })
 }
