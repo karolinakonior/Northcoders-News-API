ec895c16f65764699fc31af106ddf13dc84679f9
karolinakonior <karolina.koniorr@gmail.com>
2023-10-05 14:19:45 +0100

    Refactored app router and broke down logic into sub routers

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 const express = require("express")
 const app = express();
 
-const { getTopics,
-        getEndpoints,
-        getArticleByID,
-        getCommentsByArticleId,
-        getArticles,
-        deleteComment,
-        patchArticle,
-        postComment,
-        getUsers
-      } = require('./controllers/app.controllers')
+const apiRouter = require("./routes/api-router");
 
-app.use(express.json());
-
-app.get('/api', getEndpoints);
-
-app.get('/api/articles', getArticles)
-
-app.get('/api/topics', getTopics);
-
-app.get('/api/articles/:article_id', getArticleByID);
-
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
-
-app.post('/api/articles/:article_id/comments', postComment)
-
-app.get('/api/users', getUsers)
-
-app.delete('/api/comments/:comment_id', deleteComment)
-
-app.patch('/api/articles/:article_id', patchArticle)
+app.use('/api', apiRouter)
 
 app.all('/*', (req, res, next) => {
     res.status(404).send({ msg: 'Path not found.' });
   })
-
 
 app.use((err, req, res, next) => {
     if (err.code === '22P02' || err.code === '23502') {
