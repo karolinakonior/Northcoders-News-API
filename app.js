8a1fb80590d9b301fef38568c3fe685db4da32f2
karolinakonior <karolina.koniorr@gmail.com>
2023-10-22 18:21:34 +0100

    Added cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express();
+const cors = require('cors');
 
 const apiRouter = require("./routes/api-router");
+
+app.use(cors());
 
 app.use('/api', apiRouter)
 
